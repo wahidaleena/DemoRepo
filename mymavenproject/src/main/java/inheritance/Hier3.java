865eae35c73d83810cecc59e23ee9907c31384ad
aleena <aleena@localhost>
2025-01-02 12:10:43 +0530

    Single,Multilevel and Hierarchical Inheritance implementation done

--- mymavenproject/src/main/java/inheritance/Hier3.java
+++ mymavenproject/src/main/java/inheritance/Hier3.java
@@ -4,12 +4,16 @@ public class Hier3 extends Hier1{
 	public void displaychild2() {
 		System.out.println("Hi child2");
 	}
+	public void dis() {
+		System.out.println("Thank you");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Hier3 obj=new Hier3();
 		obj.displaychild2();
 		obj.method1();
+		obj.dis();
 		
 
 	}
